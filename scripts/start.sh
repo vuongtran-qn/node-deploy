ce8b88e22554f0b1efee1c997ae37633ccf20e5b
vuong-tran <vuong.tran11.dn@gmail.com>
2022-07-07 10:51:51 +0700

    modify script

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  #
 cat ~/.bashrc
-cd app
+cd ./app/
 ls -al
 npm install
 pm2 start app.json
