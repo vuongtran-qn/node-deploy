cecffbd53f8f32dd289b0909dbdcfffb863b474c
vuong-tran <vuong.tran11.dn@gmail.com>
2022-07-07 11:13:13 +0700

    fix error

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 echo "Hello world"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  #
 ls -al
 cd /working-space/
 npm install
