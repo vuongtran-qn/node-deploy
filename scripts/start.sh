3f1259fc0dfce8d323a8d1801310e3ec2e8cbf48
vuong-tran <vuong.tran11.dn@gmail.com>
2022-07-07 10:33:31 +0700

    fix script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo "Hello world"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  #
 cat ~/.bashrc
 cd app
 ls -al
