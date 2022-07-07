260aae10cc846d066d2c5d1e238b734d80d7d02a
vuong-tran <vuong.tran11.dn@gmail.com>
2022-07-07 14:01:31 +0700

    oke

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Hello world"
 sudo su - ec2-user
+cd
+source .bashrc
 echo $USER
-cd /home/ec2-user/my-app/
-sudo ln -s "$(which node)" /usr/bin/node
-sudo ln -s "$(which npm)" /usr/bin/npm
-/home/ec2-user/.nvm/versions/node/v16.15.1/bin/npm install
-/home/ec2-user/.nvm/versions/node/v16.15.1/bin/pm2 start app.json
+cd my-app/
+npm install
+pm2 start app.json
